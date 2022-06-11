3903461d48034a539bfe5ac2014332d05e7f24ae
AntonZyabkin <zyabkinantonv@gmail.com>
2022-06-11 18:48:01 +0300

    first commit

---
+++ TSD-11.1-UITabBarController/TSD-11.1-UITabBarController/StopWatchViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StopWatchViewController.swift
+//  TSD-11.1-UITabBarController
+//
+//  Created by Anton Zyabkin on 11.06.2022.
+//
+
+import UIKit
+
+class StopWatchViewController: UIViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TSD-11.1-UITabBarController/TSD-11.1-UITabBarController/TimeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TSD-11.1-UITabBarController
+//
+//  Created by Anton Zyabkin on 11.06.2022.
+//
+
+import UIKit
+
+class TimeViewController: UIViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
